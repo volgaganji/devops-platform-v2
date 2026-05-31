121c86c70d679bd3a336413a23ead8c363b14e6e
Volga Ganji <ganjivolga@gmail.com>
2026-05-31 19:23:03 +0530

    Add more endpoint tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,10 @@
-import sys
 import os
+import sys
 
 sys.path.append(os.path.abspath("backend"))
 
 from app import app
+
 
 def test_health():
     client = app.test_client()
@@ -11,3 +12,11 @@ def test_health():
 
     assert response.status_code == 200
     assert response.get_json()["status"] == "ok"
+
+
+def test_home():
+    client = app.test_client()
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert b"Real-Time DevOps" in response.data
